75eced2a31e373171bad1f56a901f5107b6a17e5
Vlad <166547154+GromavVL@users.noreply.github.com>
2026-01-11 18:18:42 +0200

    Add about.html

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const { readFile } = require('fs/promises');
 const http = require('http');
+const reqestListner = require('./reqestListner');
 
-const httpServer = http.createServer((request, response) => {
-  console.log('request :>> ', request);
-  console.log('response :>> ', response);
+// const httpServer = http.createServer((request, response) => {
+//   console.log('request :>> ', request);
+//   console.log('response :>> ', response);
 
-  readFile('./index.html', { encoding: 'utf-8' })
-    .then(data => {
-      response.statusCode = 200;
-      response.setHeader('Content-type', 'text/html');
-      response.end(data);
-    })
-    .catch(err => {
-      response.statusCode = 500;
-      response.setHeader('Content-type', 'text/html');
-      response.end(err);
-    });
-});
+//   readFile('./index.html', { encoding: 'utf-8' })
+//     .then(data => {
+//       response.statusCode = 200;
+//       response.setHeader('Content-type', 'text/html');
+//       response.end(data);
+//     })
+//     .catch(err => {
+//       response.statusCode = 500;
+//       response.setHeader('Content-type', 'text/html');
+//       response.end(err);
+//     });
+// });
+
+const httpServer = http.createServer(reqestListner);
 
 httpServer.listen(5000, 'localhost', () => {
   console.log('server is live :>> ');
